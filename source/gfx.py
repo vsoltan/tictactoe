7dbdbdd28c118a107394f18e2a5107bc94c712eb
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-01-11 20:16:30 -0500

    implemented the initialization of the board, filling a window of any size (although graphics.py doesn't support resizing, looking to add that for the purpose of this project)

--- source/gfx.py
+++ source/gfx.py
@@ -1,14 +1,38 @@
 from graphics import *
+
 from game import tic_tac_Game
 
 
 def main():
+    # before
+
     win = GraphWin("Tic Tac Toe", 500, 500)
     win.setBackground(color_rgb(0, 0, 0))
+    game = tic_tac_Game(3)
 
-    message = Text(Point(250, 250), "Hello!")
-    message.setTextColor("white")
-    message.draw(win)
+    print(GraphWin.getHeight(win))
+
+    #creating the board
+    for i in range(1, game.size):
+
+        horiz_grid = i * win.getWidth() / game.size
+        vert_grid = i * win.getHeight() / game.size
+
+        horizontal_bound = Line(Point(horiz_grid, 0), Point(horiz_grid, win.getHeight()))
+        vertical_bound = Line(Point(0, vert_grid), Point(win.getWidth(), vert_grid))
+        horizontal_bound.setFill("white")
+        vertical_bound.setFill("white")
+        horizontal_bound.draw(win)
+        vertical_bound.draw(win)
+
+    
+
+
+
+
+    # message = Text(Point(250, 250), "Hello!")
+    # message.setTextColor("white")
+    # message.draw(win)
 
     ## can add some transition
 
